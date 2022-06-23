78dbef0b3e4f3f6d1d7d32e61aaaec40ce19cd2a
burtonrj <burtonrj@cardiff.ac.uk>
2022-06-23 15:39:26 +0100

    Update workflow

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Ross Burton'
 author = 'Ross Burton'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
